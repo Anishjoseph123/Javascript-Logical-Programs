1e825fd237a517d8c03f5e12389890ccb2caf409
Anish Joseph <46836583+Anishjoseph123@users.noreply.github.com>
2022-05-06 16:27:04 +0530

    Add files via upload

---
+++ FindAscii.js
@@ -0,0 +1,5 @@
+//1. Finding ascii value of the first character in a string.
+
+let string = prompt("Enter a String"); //red
+let ascii = string.charCodeAt(0);
+console.log(`The ASCII value is :${ascii}`); //The ASCII value is :114
