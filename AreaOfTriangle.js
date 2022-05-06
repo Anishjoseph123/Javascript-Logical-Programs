fda8bb620b28dc3ad0bc60a60edad34569905f8e
Anish Joseph <46836583+Anishjoseph123@users.noreply.github.com>
2022-05-06 16:23:25 +0530

    Add files via upload

---
+++ AreaOfTriangle.js
@@ -0,0 +1,6 @@
+//4. Area of a Triangle. A=(base*height)/2.
+
+ let base = Number(prompt("Enter the value of base"));
+ let height = Number(prompt("Enter the height value"));
+let areaTri = (base * height) / 2;
+ console.log(`The area of the given Triangle is:${areaTri}`);
